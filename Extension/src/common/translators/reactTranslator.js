4ddee1f2c9a06f65c856bdfa760618766bc052a4
Ponchale <alfonso.hernandez@astian.org>
2023-11-01 07:58:57 -0500

    settings common

---
+++ Extension/src/common/translators/reactTranslator.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { translate } from '@adguard/translate';
+import { i18n } from './i18n';
+
+/**
+ * Retrieves localised messages by key, formats and converts into react components or string
+ */
+export const reactTranslator = translate.createReactTranslator(i18n, React);
+
+export const t = (str, message) => reactTranslator.getMessage(str, message);
